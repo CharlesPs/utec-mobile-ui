8baddc56f5378e60c20892eed1c45581ddcbc9ad
Carlos Aguinaga <andres.aguinaga@gmail.com>
2020-04-03 03:53:16 -0500

    Add Progress component

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 import Alert from './src/Components/Alert/Alert'
 import Button from './src/Components/Button/Button'
 import Picker from './src/Components/Picker/Picker'
+import Progress from './src/Components/Progress/Progress'
 
 export {
     Alert,
     Button,
     Picker,
+    Progress,
 }
---
+++ src/Components/Progress/Progress.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+
+import {
+    ActivityIndicator,
+    View,
+} from 'react-native'
+
+import { Colors } from '../../Resources/Constants/Colors'
+
+const Progress = (props) => {
+
+    const size = props.size ? props.size : 'large'
+    const color = props.color ? props.color : Colors.textGeneral
+    
+    const style = props.style ? props.style : {
+        backgroundColor: 'red',
+    }
+    
+    return (
+        <View style={style}>
+            <ActivityIndicator size={size} color={color} />
+        </View>
+    )
+}
+
+export default Progress
