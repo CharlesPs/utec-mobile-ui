1fb3ac976bbfae17ab7c47149da6af4201fc81f6
Carlos Aguinaga <andres.aguinaga@gmail.com>
2020-04-03 20:59:46 -0500

    Add Navbar component

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import Alert from './src/Components/Alert/Alert'
 import Button from './src/Components/Button/Button'
 import Loader from './src/Components/Loader/Loader'
 import MenuButton from './src/Components/MenuButton/MenuButton'
+import Navbar from './src/Components/Navbar/Navbar'
 import Picker from './src/Components/Picker/Picker'
 import Progress from './src/Components/Progress/Progress'
 
@@ -11,6 +12,7 @@ export {
     Button,
     Loader,
     MenuButton,
+    Navbar,
     Picker,
     Progress,
 }
